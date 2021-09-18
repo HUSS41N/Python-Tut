237d7cc7edf8e946e99e936be62ddc24af3c1041
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-18 22:34:06 +0530

    Create PythonTutorial1.py

---
+++ PythonTutorial1.py
@@ -0,0 +1,64 @@
+# Data types
+# integer,boolean,string,list...
+# integer
+num = 10;
+
+# string
+name = "Yatin";
+char = 'c';
+
+# float
+f = 1.77;
+# boolean true/false
+isTrue = False;
+# print(f)
+
+# lists
+nums = [1,2,3,4,5];
+# print(nums)
+
+# dictonaries key value pairs
+myDict = {"key":"Python is cool","key2":"value2"}
+# print(myDict["key"])
+
+# Tuples
+# they are immutable
+myTuple = (1,2,3,4,5);
+# print(myTuple);
+
+# sets
+# repeated values ni hoti
+mySet = {1,2,3,4,5,1,1,1};
+# print(mySet);
+
+# Strings
+# strore words/group of words
+# Indexing and slicing
+# indexing grabs single character
+myString = "Hello"
+# print(myString[0])
+# print(myString[4])
+# negative indexing starts form reverse side
+# print(myString[-1])
+
+# slicing grabs part of a string
+# syntax myString[startingIndex:endingIndex+1]
+# print(myString[1:4])
+# setpping
+# syntax myString[start:end:step+1]
+# [statindex:endIndex+1:1]
+# print(myString[0:5:2])
+
+# Bht hi kamal ka trick
+# reverse a string
+print(myString[::-1])
+
+
+
+
+
+
+
+
+
+
