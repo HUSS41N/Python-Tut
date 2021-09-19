7f643ba6ece50b86f1f22257c92720609e2c22c1
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-19 22:41:58 +0530

    Create PythonTutorial3.py

---
+++ PythonTutorial3.py
@@ -0,0 +1,36 @@
+# superheroes
+# dictonarires are key value pairs
+# {KEY:VALUE}
+myDict = {"myFav":"SuperMan","mostPowerful":"HULK","mostPopular":"SpiderMan"}
+# no indexing 
+# no slicing
+
+# accesiing values with key
+# print(myDict["myFav"])
+# print(myDict["mostPowerful"])
+# print(myDict["mostPopular"])
+
+# chaning values
+# myDict["mostPowerful"] = "THOR"
+# print(myDict["mostPowerful"])
+# myDict["myFav"] = "Batman"
+# print(myDict["myFav"])
+
+# ketting all keys
+# this will give you all the keys
+# print(myDict.keys())
+
+# getting all values
+# print(myDict.values())
+
+# getting items
+# print(myDict.items())
+
+
+myList = [[1,2,3],[{"name":"Neha","lang":"Python 3 is best"}]]
+# print(myList[0])
+# print(myList[0][2])
+print(myList[1]);
+print(myList[1][0]);
+print(myList[1][0]["name"])
+print(myList[1][0]["lang"])
