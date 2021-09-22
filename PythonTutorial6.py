71c654b66562f71f7715f6dc5a14be603fd6280a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-22 21:35:13 +0530

    Create PythonTutorial6.py

---
+++ PythonTutorial6.py
@@ -0,0 +1,31 @@
+# Type conversion
+
+# int
+num = 10;
+# print(type(num))
+# name = 'x'
+# print(type(name))
+
+# int to float and float to int
+# print(float(num))
+num2 = 19.99
+num3 = int(num2)
+print(num3)
+
+
+# name = "Neha"
+# print(int(name))
+
+stringNum = "1999"
+print(int(stringNum))
+
+num4 = 1998
+s = str(num4)
+print(type(s))
+
+# given a list remove all the duplicate elements
+# [1,2,3,1,1,2,2,3] = [1,2,3]
+# hint convert list to set and set to list
+
+# given a list count the number of 1's
+# [0,1,0,1,1,1,0] => 4
