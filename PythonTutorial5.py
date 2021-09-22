dd6af6e5703aa270e34d96335873bd0529129f22
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-22 21:34:57 +0530

    Create PythonTutorial5.py

---
+++ PythonTutorial5.py
@@ -0,0 +1,30 @@
+# set
+
+# myList = [1,2,3,1,2,3]
+# print(myList)
+
+# set
+mySet = {1,2,3,1,2,3}
+print(mySet)
+
+# add
+# inserts the value at the end 
+# similar to append in lists
+mySet.add(4);
+print(mySet)
+
+# pop removes the first index value
+mySet.pop();
+mySet.pop();
+print(mySet)
+# clear = set ko khali kr dega
+# delets all the value
+# mySet.clear()
+# print(mySet)
+
+# boolean
+
+# isTrue = True
+# print(isTrue)
+print(2>4)
+print(99>100)
