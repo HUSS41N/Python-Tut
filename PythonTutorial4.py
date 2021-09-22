e887fa874029b465d8aaf09c01e37f1d7b60e32b
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-22 21:34:35 +0530

    Create PythonTutorial4.py

---
+++ PythonTutorial4.py
@@ -0,0 +1,21 @@
+# they are similar to lists
+# but they are immuatble
+# supports indexing slicing
+# myTuble = (tup1,tup2)
+
+myTuple = (1,2,3,1,1,1)
+print(myTuple)
+# indexing
+print(myTuple[0])
+
+# number of particular element
+print(myTuple.count(1)) 
+print(myTuple.count(2)) 
+
+# index-> gives the index of an element(first instance)
+print(myTuple.index(1))
+
+# Data integraty
+# pi = 3.14
+# g = 9.8
+# speed of light =
