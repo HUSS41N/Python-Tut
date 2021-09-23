e9e129fdb6839d014753e4842b3e8954ecb0b180
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-23 22:00:01 +0530

    Create PythonTutorial7.py

---
+++ PythonTutorial7.py
@@ -0,0 +1,27 @@
+# num1 = 10;
+# num2 = 20;
+# sum = num1+ num2;
+# print(sum);
+
+# taking input from user
+# input("What is your Name? ")
+
+# storing the user input in a varible
+# myName = input("What is your Name? ")
+# print(myName) 
+
+# by default input returns a string value
+# num = input("Enter a number?")
+# print(type(num))
+
+# taking interger input from the user
+# num = input("Enter a number?")
+#  type converting str to int
+# num = int(num)
+# print(type(num))
+
+# python sexy one liner
+# num = int(input("enter a number? "))
+# print(type(num))
+
+# Q1. Take two integer from user and print their sum and product?
