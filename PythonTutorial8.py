9d2c938b533a957fcd724ae1b5ef303700c3ebdb
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-23 22:39:57 +0530

    Create PythonTutorial8.py

---
+++ PythonTutorial8.py
@@ -0,0 +1,41 @@
+# operators
+
+# = assignment operator
+# x = "Hope conty"
+# print(x)
+
+# + add
+# - subtract
+# * multiply
+# / divide
+
+# % modulo opertor
+# modulo gives you the remainder
+# print(10 % 2)
+
+# python comparisons operators
+# == -> eqaulity operator
+# gives you boolean values 
+# print(2==2)
+
+# != -> inEqaulity / not equals to
+# print(2 != 4)
+# print(2!=2)
+
+
+# logical operators
+# and operator
+# dono side s true ayega to true hoga otherwise false
+# print( (2<4) and (6<10) )
+# print( 2==2 and 4==4 )
+# print(2==4 and 4==4)
+
+# or operator
+# returns true if only one condition is true or both is true
+# print(2==2 or 4==4)
+# print(2==2 or 2==3)
+# print(2==3 or 2==4)
+
+# not operator
+# opposite
+# print(not 2==3 )
