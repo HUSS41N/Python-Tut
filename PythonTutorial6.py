544293d76e84ea3aa2f8cba77bb6d3183fcb7eca
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-23 21:41:54 +0530

    Update PythonTutorial6.py

--- PythonTutorial6.py
+++ PythonTutorial6.py
@@ -29,3 +29,47 @@ print(type(s))
 
 # given a list count the number of 1's
 # [0,1,0,1,1,1,0] => 4
+
+# type conversion
+# convert one data type into other
+num = 10.77; #flot type 
+# print(num)
+
+# Syntax for type conversion nameofvaribale = typeToBechanged(varible we want to change)
+
+# checking type
+# print(type(num))
+
+# int()
+# print(int(num))
+
+# age = 23
+# ageFloat = float(age)
+# print(ageFloat)
+
+# cake = "1998"
+# print(type(cake))
+# # string to integer
+# print(int(cake) + 2)
+
+# given a list remove all the duplicate elements
+# [1,2,3,1,1,2,2,3] = [1,2,3]
+# hint convert list to set and set to list
+
+# myList = [1,2,3,1,1,2,2,3]
+# mySet = set(myList)
+# myList = list(mySet)
+# print(myList)
+
+# given a list count the number of 1's
+# [0,1,0,1,1,1,0] => 4
+
+# myList = [0,1,0,1,1,1,0]
+# # print(myList.count(1))
+
+# myTuple = tuple(myList)
+# print(myTuple.count(1));
+
+# first intstace k liy hi kaam krega
+myTuple = (1,2,4,3,4,5,6,4);
+print(myTuple.index(4))
