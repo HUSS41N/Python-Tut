b43858e0fe6b644629defcc1e032ac11d22b0611
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-24 20:56:48 +0530

    Create PythonTutorial9.py

---
+++ PythonTutorial9.py
@@ -0,0 +1,68 @@
+# if certain condition is met only then the block will be executed
+
+# if(True):
+#     print("Hello")
+#     print("Python is cool")
+#     print("Python is cool")
+
+# print("Python is cool")
+
+# if(False):
+#     print("This will not be printed")
+
+# if(2+2 == 4):
+#     print("Printed 4")
+
+# if(2>10):
+#     print("This will not be printed")
+
+
+# if ( 4 % 2 == 0):
+#     print("Yeah ! Above condition is true")
+#     if ( 2 % 2 == 0):
+#         print("Yeah ! ..................")
+
+# num = int(input("Enter a number "))
+
+# if(num % 2 == 0):
+#     print("You won the game")
+#     if(num == 10):
+#         print("You are so Awsm")
+
+# Enter a number if the number is divisible of 3 print "fizz" if the number is divisible by "5" print "buzz"
+# if divisible by both print "fizzbuzz"
+
+# num = int(input("Enter a number ? "))
+
+# if(num % 3 == 0):
+#     print("Fizz")
+
+# if(num % 5 == 0):
+#     print("Buzz")
+
+# if(num % 3 == 0 and num % 5 == 0):
+#     print("Fizz Buzz")
+
+# if else
+# if the condition is false else block will be executed
+# if(2+2 == 6):
+#     print("Yahhhh! ...........")
+# else:
+#     print("No.............")
+
+# if elif else
+num = int(input("Enter a number ? "))
+# elif means else if
+
+if(num % 15 == 0):
+    print("Fizz Buzz")
+elif(num % 3 == 0):
+    print("Fizz")  
+elif(num % 5 == 0):
+    print("Buzz")
+else:
+    print("Always")
+
+# 1. Input a number and check whether the number is divisible by 2
+# if divisible print("Divisible")
+
