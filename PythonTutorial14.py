46c7d40ef77f4dc390e9d3d7c35002df873a7050
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-10-05 22:21:20 +0530

    Create PythonTutorial14.py

---
+++ PythonTutorial14.py
@@ -0,0 +1,23 @@
+num = 10;
+name = "Sania"
+# exception handling
+# code wll try to execute the try block if there is an error
+# the except block will handle it
+# try:
+#     print(num + name)
+# except:
+#     print("Execute if there is an error")
+
+# you can also be more efficient
+# try:
+#     print(num + name)
+# except TypeError:
+#     print("Execute if there is an error")
+
+
+# break statement
+# break will pull you out of the loop
+for _ in range(1,101):
+    if(_ == 50):
+        print(_)
+        break;
